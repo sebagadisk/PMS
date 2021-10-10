4ac2c0924e43e8f65b34683c4cc748b9ce275833
sebagadisk <sebexk3@gmail.com>
2021-10-10 21:54:56 +0300

    add list of company

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="panel panel-primary">
-    <div class="panel-heading">List of Companies</div>
-    <div class="panel-body">
-        <!-- List group -->
-        <ul class="list-group">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">List of Companies</div>
+            <div class="panel-body">
+
+            @foreach($companies as $company)
+                <!-- List group -->
+                    <ul class="list-group">
+                        <li class="list-group-item">{{ $company->name }}</li>
+                    </ul>
+                @endforeach
+            </div>
+        </div>
     </div>
-</div>
+
 @endsection
